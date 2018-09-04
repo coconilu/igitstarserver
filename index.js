8e1e108d1b94950868ae9b1673d4be30776a7bc0
alpha <827130441@qq.com>
2018-09-04 11:50:04 +0800

    build the static server

---
+++ index.js
@@ -0,0 +1,12 @@
+const Koa = require('koa');
+const static = require('koa-static')
+const path = require('path')
+const app = new Koa();
+
+const staticPath = './static'
+
+app.use(static(
+  path.join( __dirname,  staticPath)
+))
+
+app.listen(3000);
